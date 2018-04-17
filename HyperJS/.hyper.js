093ce6c5cd362ec25140709392cf41057f406b7f
Allyson Silva <allysonsilvaweb@gmail.com>
2018-04-17 20:11:52 -0300

    HyperJS - Changed [hyperpower]

--- HyperJS/.hyper.js
+++ HyperJS/.hyper.js
@@ -148,6 +148,7 @@ module.exports = {
     //   `@company/project`
     //   `project#1.0.1`
     plugins: [
+        // 'hyperpower',
         'hyperborder',
         'hyper-statusline',
         'hyper-tabs-enhanced',
@@ -171,7 +172,7 @@ module.exports = {
     // in development, you can create a directory under
     // `~/.hyper_plugins/local/` and include it here
     // to load it and avoid it being `npm install`ed
-    localPlugins: ['hyperpower'],
+    localPlugins: [],
 
     keymaps: {
         // Example
